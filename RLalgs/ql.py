6b837e7f4e8cdb2ee084bb097175cb31770a69d5
Jimenius <l.minhui@qq.com>
2018-10-04 20:40:08 -0400

    ql docs

--- RLalgs/ql.py
+++ RLalgs/ql.py
@@ -7,7 +7,25 @@ def QLearning(env, num_episodes, gamma, lr, e):
     Implement the Q-learning algorithm following the epsilon-greedy exploration. Update Q at the end of every episode.
 
     Inputs:
-    
+    env: OpenAI Gym environment 
+            env.P: dictionary
+                    P[state][action] are tuples of tuples tuples with (probability, nextstate, reward, terminal)
+                    probability: float
+                    nextstate: int
+                    reward: float
+                    terminal: boolean
+            env.nS: int
+                    number of states
+            env.nA: int
+                    number of actions
+    num_episodes: int
+            Number of episodes of training
+    gamma: float
+            Discount factor.
+    lr: float
+            Learning rate.
+    e: float
+            Epsilon value used in the epsilon-greedy method.
 
     Outputs:
     Q: numpy.ndarray
